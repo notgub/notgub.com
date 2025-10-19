290f16a88d0fe51ee8003dd511a5caab1f7eacea
notgub <notgub.info@gmail.com>
2025-10-19 15:21:00 +0700

    chore: change path

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -5,7 +5,7 @@ import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
+import "./../globals.css";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -55,7 +55,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <TooltipProvider delayDuration={0}>
+            {children}
+            <Navbar />
+          </TooltipProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,11 +2,11 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Chanchai Thaiyanon",
+  initials: "CT",
+  url: "https://notgub.com",
+  location: "Chiang Mai, TH",
+  locationLink: "https://www.google.com/maps/place/Chiang+Mai",
   description:
     "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
   summary:

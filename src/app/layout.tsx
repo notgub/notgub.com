728edaa59840c6e88d76d792246d43363ffad0af
notgub <notgub.info@gmail.com>
2025-10-19 16:19:49 +0700

    feat: analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -56,6 +57,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
